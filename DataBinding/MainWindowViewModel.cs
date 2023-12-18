ed5c4666567c615784fb55a2ae9c09bcca1ba7fd
Kayla <kehuber@students.ntc.edu>
2023-12-18 13:14:07 -0600

    Add files for data binding pattern example

---
+++ DataBinding/MainWindowViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBinding
+{
+	public class MainWindowViewModel : INotifyPropertyChanged
+	{
+		private int boundNumber;
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public int BoundNumber
+		{
+			get
+			{
+				return boundNumber;
+			}
+			set
+			{
+				if (boundNumber != value)
+				{
+					boundNumber = value;
+					OnPropertyChanged("BoundNumber");
+				}
+			}
+		}
+
+		public void OnPropertyChanged(string propertyName)
+		{
+			if (PropertyChanged != null)
+			{
+				PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+	}
+}
